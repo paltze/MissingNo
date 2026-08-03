46628c57b17746b9c69ac1cf42ef9a78e8b972bf
Divyansh Deep <manmica2018@gmail.com>
2026-08-03 16:37:42 +0530

    Reading ROM

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#define u8 unsigned char
+
+int main() {
+    u8 memory[65536];
+
+    FILE* ROM = fopen("./PokemonRed.gb", "rb");
+    FILE* fp = fopen("./out.txt", "w");
+
+    u8 byte;
+    size_t i = 0;
+
+    while (fread(&byte, 1, 1, ROM) == 1) {
+        fprintf(fp, "%02X\n", byte);
+    }
+
+    fclose(ROM);
+    fclose(fp);
+
+    return 0;
+}
